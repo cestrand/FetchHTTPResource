ee8bd7a5fce8395be027ff4c28693bb8f833ab4f
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-10-24 23:36:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace FetchHTTPResource
+{
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+
+            string pageBody = await FetchPage("https://example.com");
+            Console.WriteLine(pageBody);
+
+            string pageBody2 = await FetchPage("https://exampleeee.com");
+            Console.WriteLine(pageBody2);
+        }
+
+        static async Task<string> FetchPage(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // above three commends can be replaced with method below:
+                // string responseBody = await client.GetStringAsync(url);
+
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("\nException occured:");
+                Console.WriteLine("Message: {0} ", ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
